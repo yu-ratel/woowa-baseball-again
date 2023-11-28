3e2d9d5725c0f0eca5e87473bb5451fefebd19bf
yu-ratel <dbdudals100@naver.com>
2023-11-28 13:51:54 +0900

    feat: 난수 생성 기능구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Random } from '@woowacourse/mission-utils';
+
 class App {
-  async play() {}
+  async play() {
+  }
+
+  randomNumberGenerator(min, max, count) {
+    const numbers = [];
+    while (numbers.length < count) {
+      const number = Random.pickNumberInRange(min, max);
+      if (numbers.includes(number)) {
+        continue;
+      }
+      numbers.push(number);
+    }
+  }
+  
+  startGuide() {
+
+  }
 }
 
 export default App;
