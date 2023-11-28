36d6950cb3e94e188881e51a6cf104e38df2eff5
yu-ratel <dbdudals100@naver.com>
2023-11-28 13:54:41 +0900

    feat: 게임시작 안내하는 기능구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Random } from '@woowacourse/mission-utils';
+import { Random, Console } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
+    this.#startGuide();
   }
 
   randomNumberGenerator(min, max, count) {
@@ -13,11 +14,15 @@ class App {
       }
       numbers.push(number);
     }
-  }
-  
-  startGuide() {
 
+    return numbers;
+  }
+
+  #startGuide() {
+    Console.print('숫자 야구 게임을 시작합니다.\n');
   }
 }
 
 export default App;
+let a = new App();
+a.play();
